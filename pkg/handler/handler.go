0d6cc649057540349b73f105961b793aae744d25
athunlal <athunlalp@gmail.com>
2023-11-06 01:09:37 +0530

    handler function completed, debug pending

--- pkg/handler/handler.go
+++ pkg/handler/handler.go
@@ -1,17 +1,63 @@
 package handler
 
-func GetUser() {
+import (
+	"net/http"
 
+	"github.com/athunlal/serverless/pkg/user"
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var ErrorMethodNotAllowed = "method not allowed"
+
+type ErrorBody struct {
+	ErrorMsg *string `json:"error,omitempty"`
 }
-func CreateUser() {
 
+func GetUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayCustomAuthorizerResponse, error) {
+	email := req.QueryStringParameters["email"]
+	if len(email) > 0 {
+		result, err := user.FetUser(email, tableName, dynaClient)
+		if err != nil {
+			return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+		}
+		return apiResponse(http.StatusBadRequest, result)
+	}
+
+	result, err := user.FetchUsers(tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody{aws.String(err.Error())})
+	}
+	return apiResponse(http.StatusOK, result)
 }
-func UpdateUser() {
-
+func CreateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayCustomAuthorizerResponse, error) {
+	result, err := user.CreateUser(req, tableName, dynaClient)
+	if err != nil {
+		return apiResponse(http.StatusBadRequest, ErrorBody){
+			aws.String(err.Error()),
+		}
+	}
+	return apiRespse(http.StatusCreated, result)
 }
-func DeleteUser() {
-
+func UpdateUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayCustomAuthorizerResponse, error) {
+	result , err := user.UpdateUser(req, tableName, dynaClient)
+	if err != nil{
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error())
+		})
+	}
+	return apiResponse(http.StatusOK, result)
 }
-func UnhandleMethod() {
-
+func DeleteUser(req events.APIGatewayProxyRequest, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*events.APIGatewayCustomAuthorizerResponse, error) {
+	err :=user.DeleteUser(req, tableName, dynaClient)
+	if err != nil{
+		return apiResponse(http.StatusBadRequest, ErrorBody{
+			aws.String(err.Error())
+		})
+	}
+	return apiResponse(http.StatusOK, nil)
+}
+func UnhandleMethod() (*events.APIGatewayProxyResponse, error) {
+	return apiResponse(http.StatusMethodNotAllowed, ErrorMethodNotAllowed)
 }
--- pkg/user/user.go
+++ pkg/user/user.go
@@ -1,3 +1,61 @@
 package user
 
-var ()
+import (
+	"errors"
+
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbiface"
+)
+
+var (
+	ErrorFaledToFetchRecord = "failed to fetch record"
+)
+
+type User struct {
+	Email     string `json:"email"`
+	FirstName string `json:"firstname"`
+	LastName  string `json:"lastname"`
+}
+
+func FetUser(email, tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*User, error) {
+	input := &dynamodb.GetItemInput{
+		Key: map[string]*dynamodb.AttributeValue{
+			"email": {
+				S: aws.String(email),
+			},
+		},
+		TableName: aws.String(tableName),
+	}
+	result, err := dynaClient.GetItem(input)
+	if err != nil {
+		return nil, errors.New(ErrorFaledToFetchRecord)
+	}
+
+	item := new(User)
+	err = dynamodbattribute.UnmarshalMap(result.Item, item)
+	if err != nil {
+		return nil, errors.New(ErrorFailedToUnmarshalRecord)
+	}
+}
+
+func FetchUsers(tableName string, dynaClient dynamodbiface.DynamoDBAPI) (*[]User, error) {
+	input := &dynamodb.ScanInput{
+		TableName: aws.String(tableName),
+	}
+
+	result, err := dynaClient.Scan(input)
+	if err != nil {
+		return nil, errors.New(ErrorFaiedToFetchRecord)
+	}
+	item := new([]User)
+	err = dynamodbattribute.UnmarshalMap(result.Items, item)
+	return item, nil
+}
+
+func CreateUser() {}
+
+func UpdateUser() {}
+
+func DeleteUser() error {}
